8c20a4ae0240ed47ac1008670921f96d3c0047b2
Sebastian Günther <sguenther3141@gmail.com>
2021-04-27 00:46:59 +0200

    add password check

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+
+var jsonParser = bodyParser.json()
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({connection: 'OK'})
+})
+
+app.post('/kuchen/password', jsonParser, (req, res) => {
+  try {
+    const pw = req.body.password
+    console.log(pw)
+    if (pw === 'test') {
+      res.json({
+        link: "test",
+      })
+    } else {
+      return res.status(400).json({ error: 'wrong password' })
+    }
+  } catch {
+    return res.status(400).json({ error: 'no password sent' })
+  }
 })
 
 app.listen(port, () => {
